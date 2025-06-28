9a096a87ceb7021e6151e5206f05a742501efdf1
rei <mucajrei5@gmail.com>
2025-06-29 11:32:07 +1200

    City service

--- src/main/java/CityService.java
+++ src/main/java/CityService.java
@@ -1,14 +1,12 @@
-
 import com.example.travel_agency.entities.City;
 
 import java.util.List;
 import java.util.Optional;
 
-    public interface CityService {
-        City createCity(City city);
-        List<City> getAllCities();
-        Optional<City> getCityById(Long id);
-        City updateCity(Long id, City city);
-        void deleteCity(Long id);
-    }
-
+public interface CityService {
+    City createCity(City city);
+    List<City> getAllCities();
+    Optional<City> getCityById(Long id);
+    City updateCity(Long id, City city);
+    void deleteCity(Long id);
+}
