9483049ccbfe97a07e5b4b0b87f334cae052f8a5
rafaelaa7253 <rafaela.rakipi29@gmail.com>
2025-06-29 12:54:13 +0200

    committed airport and hotel Services,Repositories, Impl

--- src/main/java/com/example/travel_agency/repositories/AirportRepository.java
+++ src/main/java/com/example/travel_agency/repositories/AirportRepository.java
@@ -1,9 +1,13 @@
 package com.example.travel_agency.repositories;
 
 import com.example.travel_agency.entities.Airport;
+import com.example.travel_agency.entities.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-
-
+@Query(value = "Select a from Airport a where lower(a.city)= lower(:city)")
+List<Airport> findByCity(City city);
 }
