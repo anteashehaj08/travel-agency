12fcec556c8c5a3dde300c4fc02906d2ae35deb9
rei <mucajrei5@gmail.com>
2025-06-29 12:21:39 +1200

    City service

--- src/main/java/com/example/travel_agency/service/CityService.java
+++ src/main/java/com/example/travel_agency/service/CityService.java
@@ -1,4 +1,15 @@
 package com.example.travel_agency.service;
 
+import com.example.travel_agency.entities.City;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CityService {
+
+    City createCity(String name, Long nationalityId);
+
+    List<City> getAllCities();
+    City getCityById(Long id);
+    City updateCity(String name, Long cityId, Long countryId);
 }
