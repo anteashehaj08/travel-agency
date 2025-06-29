ced71d186d51214f15e6665cce2cbb3b5ffd8f85
Antea Shehaj <anteashehaj08@hotmail.com>
2025-06-29 16:50:31 +0200

    Commited deleting redundant code in ArrivalLoc and DepartureLoc after deleting redundant DTOs

--- src/main/java/com/example/travel_agency/entities/DepartureLoc.java
+++ src/main/java/com/example/travel_agency/entities/DepartureLoc.java
@@ -2,15 +2,9 @@ package com.example.travel_agency.entities;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @Embeddable
 public class DepartureLoc {
     @ManyToOne(optional = false)
